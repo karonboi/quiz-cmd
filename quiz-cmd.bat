47f0a8189dd41cc917476bdf63380d1d203906ec
karonboi <94039272+karonboi@users.noreply.github.com>
2021-11-10 14:48:29 +0700

    Update quiz-cmd.bat

--- quiz-cmd.bat
+++ quiz-cmd.bat
@@ -1,3 +1,22 @@
 @echo off
-mode 80,25
 title quiz-cmd
+
+:ifexist_startup
+rem See if the user have canceled the startup screen.
+rem If yes, the command will jump right to the menu.
+rem If no, you guess it :)
+if exist .startup goto startup
+if not exist .startup goto menu
+goto ifexist_startup
+
+:startup
+echo ________  ___  ___  ___  ________                ________  _____ ______   ________     
+echo |\   __  \|\  \|\  \|\  \|\_____  \              |\   ____\|\   _ \  _   \|\   ___ \    
+echo \ \  \|\  \ \  \\\  \ \  \\|___/  /| ____________\ \  \___|\ \  \\\__\ \  \ \  \_|\ \   
+echo  \ \  \\\  \ \  \\\  \ \  \   /  / /|\____________\ \  \    \ \  \\|__| \  \ \  \ \\ \  
+echo   \ \  \\\  \ \  \\\  \ \  \ /  /_/_\|____________|\ \  \____\ \  \    \ \  \ \  \_\\ \ 
+echo    \ \_____  \ \_______\ \__\\________\             \ \_______\ \__\    \ \__\ \_______\
+echo     \|___| \__\|_______|\|__|\|_______|              \|_______|\|__|     \|__|\|_______|
+echo           \|__|                                                                         
+echo.
+echo 
